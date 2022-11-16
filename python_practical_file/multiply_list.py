ed69491475ad8223d29f01958dc98c33bf5b4ac4
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-11-16 11:10:37 +0530

    ini

---
+++ python_practical_file/multiply_list.py
@@ -0,0 +1,8 @@
+list1 = [10, 20, 30, 40]
+length = len(list1)
+mul = 1
+for i in range(0, length):
+    mul = mul * list1[i]
+print("The multiplication of elements is", mul)
+
+
---
+++ python_practical_file/sum_of_list.py
@@ -0,0 +1,7 @@
+list1 = [10, 24, 45, 67, 91]
+length = len(list1)
+summ = 0
+for i in range(0, length):
+    summ = summ + list1[i]
+print("The sum of elements is", summ)
+
