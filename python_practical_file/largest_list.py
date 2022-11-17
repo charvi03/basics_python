a0cdf309cb74ae6f094e5dc81f48a522f540e58b
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-11-17 09:40:41 +0530

    ini

---
+++ python_practical_file/largest_list.py
@@ -0,0 +1,10 @@
+list2 = []
+num = int(input("Enter the number of elements"))
+for i in range(1, num + 1):
+    elements = int(input("Enter the elements"))
+    list2.append(elements)
+largest = max(list2)
+print("The largest elements in a list is", largest)
+
+
+
