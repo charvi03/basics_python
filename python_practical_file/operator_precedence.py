412713d60db7c20a51790d4f4d05e311cc6c901e
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-11-17 10:23:47 +0530

    ini

--- python_practical_file/operator_precedence.py
+++ python_practical_file/operator_precedence.py
@@ -1,3 +1,13 @@
 exp = 6 * 3 + 4 ** 2 // 5 - 8
+# step1 : 4**2=16
+# 6*3+16//5-8
+# step2 :6*3=18
+# 18+16//5-8
+# step3 : 16//5=3
+# 18+3-8
+# step4 : 18+3=21
+# 21-8
+# step5:  result=13
 print("Solved expression is", exp)
+
 
