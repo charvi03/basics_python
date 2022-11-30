e638bb2ea2025f577b55fa80c77d43dee6113461
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-11-30 11:17:09 +0530

    ini

---
+++ dictionary/prog8.py
@@ -0,0 +1,9 @@
+# adding elmnts
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+thisdict.update({"color": "red"})
+print(thisdict)
---
+++ dictionary/prog9.py
@@ -0,0 +1,21 @@
+# inserting
+Dict = {}
+print("Empty Dictionary: ")
+print(Dict)
+Dict[0] = "k"
+Dict[2] = 'For'
+Dict[3] = 1
+print("\nDictionary after adding 3 elements: ")
+print(Dict)
+# adding set of values
+Dict['Value_set'] = 2, 3, 4
+print("\nDictionary after adding 3 elements: ")
+print(Dict)
+# updating values
+Dict[2] = 'Welcome'
+print("\nUpdated key value: ")
+print(Dict)
+# nested values
+Dict[5] = {'Nested': {1: 'Life', 2: 'Geeks'}}
+print("\nAdding a Nested Key: ")
+print(Dict)
