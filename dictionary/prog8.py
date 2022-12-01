d8e73d448a4d377f72c26fe0ff2158d10be7469a
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-12-01 10:21:20 +0530

    ini

--- dictionary/prog8.py
+++ dictionary/prog8.py
@@ -8,3 +8,6 @@ thisdict = {
 thisdict.update({"color": "red"})
 print(thisdict)
 print("the model of car is ", thisdict.get('model'))
+print('car info', sorted(thisdict.items()))
+car_str = str(thisdict)
+print('Format of car datatype', type(car_str))
