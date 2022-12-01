a5e1389809ba81a64d81afaeda7a2d440b0fd908
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-12-01 10:14:48 +0530

    ini

---
+++ dictionary/prog10.py
@@ -0,0 +1,3 @@
+dictionary = {'a': 1, 'b': 2, 'c': 3}
+for key, value in dictionary.items():
+    print(key, ':', value)
---
+++ dictionary/prog11.py
@@ -0,0 +1,4 @@
+Employee = {"Name": "Charvi", "Age": 22, "salary": 25000, "Company": "GOOGLE"}
+print(Employee)
+Employee.clear()
+print(Employee)
