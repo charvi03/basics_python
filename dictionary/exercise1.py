f28fe9dd83b5f45288fcd260181547f0dab2f5fa
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-12-18 11:53:35 +0530

    ini

---
+++ dictionary/exercise1.py
@@ -0,0 +1,9 @@
+Adict = {'Mon': 3, 'Tue': 5, 'Wed': 6, 'Thu': 9}
+print("The given dictionary : ", Adict)
+check_key = "Fri"
+if check_key in Adict:
+    print(check_key, "is Present.")
+else:
+    print(check_key, " is not Present.")
+
+  
---
+++ dictionary/exercise3.py
@@ -0,0 +1,10 @@
+dict1 = {1: "Mon", 2: "Tue", 3: "Wed"}
+dict2 = {}
+# Given dictionaries
+print("The original dictionary : ", (dict1))
+print("The original dictionary : ", (dict2))
+# Check if dictionary is empty
+print("Is dict1 empty? :", bool(dict1))
+print("Is dict2 empty? :", bool(dict2))
+
+
