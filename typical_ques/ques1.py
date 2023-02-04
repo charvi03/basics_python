802e12ffa7d017d080b095ebad4af7b5c5981b0c
charvi03 <67728901+charvi03@users.noreply.github.com>
2023-02-04 11:21:19 +0530

    ini

--- typical_ques/ques1.py
+++
@@ -1,9 +0,0 @@
-string = "weather is awesome"
-# reversing words in a given string
-s = string.split()[::-1]
-l = []
-for i in s:
-    # apending reversed words to l
-    l.append(i)
-# printing reverse words
-print(" ".join(l))
